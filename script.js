df9f90f7b558e965f5f1781cd6f0f4d8f5bdf30c
Samuel Sepeši <samuel.sepesi@student.tuke.sk>
2019-05-06 17:02:38 +0200

    Css update

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function getTransitInfo() {
       mapRoute: {
         arrival_stop: 'Staničné námestie',
         arrival_time: '4:03pm',
-        departure_time: '3:53pm',
+        departure_time: '2',
         departure_stop: 'Technická univerzita',
         lineNumber: '19',
       },
@@ -44,7 +44,7 @@ function getTransitInfo() {
       mapRoute: {
         arrival_stop: 'Barca, kostoly',
         arrival_time: '4:29pm',
-        departure_time: '4:02pm',
+        departure_time: '6',
         departure_stop: 'Technická univerzita',
         lineNumber: '12',
       },
@@ -55,7 +55,7 @@ function getTransitInfo() {
       mapRoute: {
         arrival_stop: 'Staničné námestie',
         arrival_time: '4:03pm',
-        departure_time: '3:53pm',
+        departure_time: '8',
         departure_stop: 'Technická univerzita',
         lineNumber: '19',
       },
@@ -66,7 +66,7 @@ function getTransitInfo() {
       mapRoute: {
         arrival_stop: 'Nám. Maratónu mieru',
         arrival_time: '3:53pm',
-        departure_time: '3:49pm',
+        departure_time: '11',
         departure_stop: 'Amfiteáter',
         lineNumber: '6',
       },
@@ -77,7 +77,7 @@ function getTransitInfo() {
       mapRoute: {
         arrival_stop: 'Nám. osloboditeľov',
         arrival_time: '4:12pm',
-        departure_time: '3:53pm',
+        departure_time: '5',
         departure_stop: 'Amfiteáter',
         lineNumber: '6',
       },
@@ -88,7 +88,7 @@ function getTransitInfo() {
       mapRoute: {
         arrival_stop: 'Stará nemocnica',
         arrival_time: '4:11pm',
-        departure_time: '3:58pm',
+        departure_time: '10',
         departure_stop: 'Poliklinika Sever',
         lineNumber: '4',
       },
@@ -98,14 +98,19 @@ function getTransitInfo() {
   wrapper.addClass('wrapper');
   response.map((transit) => {
     const { mapRoute, name } = transit;
-    const bus = `<div class="cisloAutobusu">${mapRoute.lineNumber}</div><hr />`;
-    const leaveTime = `<div class="casOdchodu">${mapRoute.departure_time}</div><hr />`;
+    const bus = `<div class="cisloAutobusu">${mapRoute.lineNumber}</div><br />`;
+    const leaveTime = `<div class="casOdchodu">o <b>${
+      mapRoute.departure_time
+    }</b> minúty</div><br />`;
+    const numberTimeWrapper = $('<div/>');
+    numberTimeWrapper.addClass('numberTimeWrapper');
+    numberTimeWrapper.append(bus, leaveTime);
     const stop = `<div class="zastavka">${
       mapRoute.departure_stop
-    } --> </div><hr /><div class="zastavka"> ${name} </div><hr /><hr/>`;
+    } --> </div><br /><div class="zastavka"> ${name} </div><br /><br/>`;
     const content = $('<div/>');
     content.addClass('content');
-    content.append(bus, leaveTime, stop);
+    content.append(numberTimeWrapper, stop);
     wrapper.append(content);
   });
   const header = `<h1 class="header">MHD app</h1>`;
